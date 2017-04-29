3ab443a8a3310b73890b09c923d4961c953b32a9
haiyang <haiyang@imenu.local>
2017-04-29 12:47:54 -0400

    d3 and ng

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 console.info('hello');
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('GreetingController', ['$scope', function($scope) {
+                                                    $scope.greeting = 'Hola!';
+                                                    var body = d3.select("#d3");
+                                                    var div = body.append("div");
+                                                    div.html("Hello, world!");
+
+                                                  }]);
